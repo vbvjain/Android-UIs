75002b1d5f618ee9070da7c000e13bb3c3d97057
Vaibhav Jain <vaibhavjain89@live.in>
2018-01-11 15:35:42 +0530

    changes

--- app/src/main/java/com/vbvjain/android_uis/loginscreens/LoginScreen1.java
+++ app/src/main/java/com/vbvjain/android_uis/loginscreens/LoginScreen1.java
@@ -1,7 +1,9 @@
 package com.vbvjain.android_uis.loginscreens;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vbvjain.android_uis.R;
 
 public class LoginScreen1 extends AppCompatActivity {
 
