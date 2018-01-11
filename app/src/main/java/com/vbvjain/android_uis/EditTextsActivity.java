da94fd30f26b94de05db6027f48bcb52574df52d
Vaibhav Jain <vaibhavjain89@live.in>
2018-01-11 11:43:05 +0530

    different edit texts

--- app/src/main/java/com/vbvjain/android_uis/EditTextsActivity.java
+++ app/src/main/java/com/vbvjain/android_uis/EditTextsActivity.java
@@ -3,11 +3,11 @@ package com.vbvjain.android_uis;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EditTextsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edittexts);
     }
 }
