2622cf7a7c0ee41138badfd267c537c37813316e
Vaibhav Jain <vaibhavjain89@live.in>
2018-01-11 15:10:13 +0530

    creates login screen 1

---
+++ app/src/main/java/com/vbvjain/android_uis/loginscreens/LoginScreen1.java
@@ -0,0 +1,13 @@
+package com.vbvjain.android_uis.loginscreens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen1);
+    }
+}
